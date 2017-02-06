31737576981c6702230256bc9a6421bdc9d45a1d
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2017-02-06 17:31:26 +0000

    typed actions

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,8 +1,12 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
 const INITIAL_STATE = { email: '' }; // Ensure state is never undefined
 
 export default(state = INITIAL_STATE, action) => {
   switch (action.type) {
-
+    case EMAIL_CHANGED:
+      console.log(EMAIL_CHANGED);
+      return state;
     default:
       return state;
   }
