4c8f3335e27452540eb32099342b6ff2b67cbed5
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2017-02-06 12:22:13 +0000

    setting app action creator

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = { 'email': ''}; // Ensure state is never undefined
+
+export default(state = INITIAL_STATE, action) => {
+  switch(action.type) {
+
+    default:
+      return state;
+  }
+};
